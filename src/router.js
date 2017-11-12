97bcbaebecfd67d6e8ee86f6daf024a68f959c5b
Jaden Goodwin <jaden.goodwin24@gmail.com>
2017-11-11 20:24:34 -0700

    files

---
+++ src/router.js
@@ -0,0 +1,11 @@
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Destinations from './components/Destinations/Destinations';
+import Shop from './components/Shop/Shop';
+import Trailers from './components/Trailers/Trailers';
+
+// export default (
+//     <Switch>
+//         <Route component={} 
+// )
