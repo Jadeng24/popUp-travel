b089d726faf4021fcbf8f8faf4f537db28bbc3db
Jaden Goodwin <jaden.goodwin24@gmail.com>
2018-01-02 18:03:41 -0700

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import './Footer.css';
+
 import { Link } from 'react-router-dom';
+import './Footer.css';
 import facebook from './facebook.png';
 import instagram from './instagram.png';
 import pinterest from './pinterest.png';
